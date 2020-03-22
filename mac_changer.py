26ff6ffb0e371b32bb98e8491ab010afaa6b72a7
root <root@kali>
2020-03-22 18:02:17 +0530

    Independent Variables

--- mac_changer.py
+++ mac_changer.py
@@ -1,5 +1,8 @@
 import subprocess
 
-subprocess.call("ifconfig wlan0 down", shell=True)
-subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:55", shell=True)
-subprocess.call("ifconfig wlan0 up", shell=True)
+int_face = input("Enter the interface name whose mac address is to be changed:")
+mac_id = input("Enter the mac id u want to change:")
+subprocess.call("ifconfig " + int_face + " down", shell=True)
+subprocess.call("ifconfig " + int_face + " hw ether " + mac_id, shell=True)
+subprocess.call("ifconfig " + int_face + " up", shell=True)
+print("Command Executed!")
