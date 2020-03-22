eb4c93de5c591ab7a753a0860c8f62ab6f13700c
root <root@kali>
2020-03-22 21:28:33 +0530

    Secured

--- mac_changer.py
+++ mac_changer.py
@@ -2,7 +2,7 @@ import subprocess
 
 int_face = input("Enter the interface name whose mac address is to be changed:")
 mac_id = input("Enter the mac id u want to change:")
-subprocess.call("ifconfig " + int_face + " down", shell=True)
-subprocess.call("ifconfig " + int_face + " hw ether " + mac_id, shell=True)
-subprocess.call("ifconfig " + int_face + " up", shell=True)
+subprocess.call(["ifconfig", int_face, "down"])
+subprocess.call(["ifconfig", int_face, "hw", "ether", mac_id])
+subprocess.call(["ifconfig", int_face, "up"])
 print("Command Executed!")
